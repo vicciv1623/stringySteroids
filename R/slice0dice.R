ff95490a5d06869c3a995b67b3dcffd88ef67876
vicciv1623 <vickyhaeunjung@gmail.com>
2025-10-24 13:40:19 -0400

    add help manual for splice0dice

--- R/slice0dice.R
+++ R/slice0dice.R
@@ -1,3 +1,17 @@
+#' Split a string based on given indices
+#' @param x A string
+#' @param split A numerical vector indicating which indices of x to be split on
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' x<-"Happy birthday to me"
+#' split<-c(2,9)
+#' splice0dice(x, split)
+#'
+
+
 slice0dice<-function(x, split){
   # input
   # x is the string you wish to slice
